1d191388b2450cce0791dcb8b9c5b293fe15a7d4
Barbara Choy <barbchoy@gmail.com>
2025-01-14 22:42:11 -0800

    server update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const app = express();
+const port = process.env.port || 3001;
+
+// Middlewares
+app.use(express.json());
 app.use(cors());
+
+
+// Routes
+
+
+// Error handling  middleware
+
 
 // Set up file storage  
 const storage = multer.diskStorage({
-    destination: './uploads/',
+    destination: 'uploads/',
     filename: (req, file, cb) => {
         cb(null, file.originalname);
     }
@@ -23,8 +37,9 @@ app.post('/upload', upload.single('pdf'), (req, res) => {
 
 // Endpoint to retrieve PDF  
 app.get('/pdf/:filename', (req, res) => {
-    const filePath = path.join(__dirname, 'uploads', req.params.filename);
+    const filePath = path.join(__dirname, '/uploads', req.params.filename);
     res.sendFile(filePath);
 });
 
-app.listen(4200, () => console.log('Server running on port 4200'));
+// Server running 
+app.listen(port, () => console.log(`Server running on port ${port}`));
